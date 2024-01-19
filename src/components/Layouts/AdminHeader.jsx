56af275d84f6813f59684e7c263037bb91c0e486
Poojan-innovatemr <107676798+poojan0103@users.noreply.github.com>
2024-01-19 20:09:01 +0530

    Student Profile Page

--- src/components/Layouts/AdminHeader.jsx
+++ src/components/Layouts/AdminHeader.jsx
@@ -19,51 +19,7 @@ import { IconButton } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import MenuIcon from "@mui/icons-material/Menu";
-
-// const Search = styled('div')(({ theme }) => ({
-//     position: 'relative',
-//     borderRadius: theme.shape.borderRadius,
-//     backgroundColor: alpha(theme.palette.common.white, 0.15),
-//     '&:hover': {
-//       backgroundColor: alpha(theme.palette.common.white, 0.25),
-//     },
-//     marginRight: theme.spacing(2),
-//     marginLeft: 0,
-//     width: '100%',
-//     [theme.breakpoints.up('sm')]: {
-//       marginLeft: theme.spacing(3),
-//       width: 'auto',
-//     },
-//     background: '#FAFBFC',
-//     border: '1px solid #445FD2',
-//   }));
-
-//   const SearchIconWrapper = styled('div')(({ theme }) => ({
-//     padding: theme.spacing(0, 2),
-//     height: '100%',
-//     position: 'absolute',
-//     pointerEvents: 'none',
-//     display: 'flex',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   }));
-
-//   const StyledInputBase = styled(InputBase)(({ theme }) => ({
-//     color: 'inherit',
-//     '& .MuiInputBase-input': {
-//       padding: theme.spacing(1, 1, 1, 0),
-//       // vertical padding + font size from searchIcon
-//       paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-//       transition: theme.transitions.create('width'),
-//       width: '100%',
-//       [theme.breakpoints.up('md')]: {
-//         width: '70ch',
-//       },
-//     },
-//   }));
-
-// const pages = ['Home', 'About', 'Marketplace','Contact Us'];
-
+import { Link } from "react-router-dom";
 const AdminHeader = ({ isExpanded, toggleSidebar }) => {
   const role = Cookies.get("name");
   const navigate = useNavigate();
@@ -103,7 +59,7 @@ const AdminHeader = ({ isExpanded, toggleSidebar }) => {
             <IconButton color="inherit">
               <NotificationsIcon />
             </IconButton>
-            <IconButton color="inherit">
+            <IconButton color="inherit" component={Link} to="/userprofile">
               <AccountCircleIcon />
             </IconButton>
             <Typography sx={{ mr: 2, color: "#6B7A99", fontSize: "14px" }}>
