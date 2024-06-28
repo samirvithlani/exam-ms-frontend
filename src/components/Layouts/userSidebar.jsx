822376cb211f6b389392b3a9b491c99a35629f3b
samirvithlani <samir.vithlani83955@gmail.com>
2024-06-28 17:15:12 +0530

    ui change user history

--- src/components/Layouts/userSidebar.jsx
+++ src/components/Layouts/userSidebar.jsx
@@ -136,6 +136,7 @@ export const UserSideBar = () => {
                 to={res.linkUrl !== "null" ? res.linkUrl : "#"}
                 onClick={() => isMobile && setIsExpanded(false)}
                 sx={{
+                  fontWeight: "bold",
                   "&:hover": {
                     backgroundColor: constant.backgroundColor,
                   },
